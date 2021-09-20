41eedacbb3fdb8a68e64db2c18ca7d47efc08098
Shamesh Krishna <krishnashamesh@gmail.com>
2021-09-20 15:37:22 +0530

    Initial commit with base structure

---
+++ src/main/java/com/svadhyaya/backend/SvadhyayaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.svadhyaya.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvadhyayaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvadhyayaBackendApplication.class, args);
+	}
+
+}
