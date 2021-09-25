cd944bafcd7c406d3b6d19c065b5c81e927873b0
Shamesh Krishna <krishnashamesh@gmail.com>
2021-09-25 14:51:40 +0530

    Cleanup for Spot Pricing

--- src/main/java/com/svadhyaya/backend/repository/HistoricalSpotPricesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.svadhyaya.backend.repository;
-
-import com.svadhyaya.backend.db.models.HistoricalSpotPricesModel;
-import com.svadhyaya.backend.repository.template.SvadhyayaRepository;
-
-public interface HistoricalSpotPricesRepository extends SvadhyayaRepository<HistoricalSpotPricesModel, Long> {
-}
